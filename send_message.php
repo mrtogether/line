65211a9674cfba3ab7722670a50e41e1d3f28dd5
mrtogether <mrtogether.ch@gmail.com>
2017-07-12 16:46:18 +0700

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -5,56 +5,29 @@
 
 $access_token = 'xjGIR1MZNjzmCI9qagfTX7ksvvmLJYmOZZfCaAvY52kld2Hm4SeDJtzRv31ZDyIum31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDtKyONakxR1kk6ADFzu3Ko5WWqxfhMcufHu3ldcWEhREAdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-    // Loop through each event
-    foreach ($events['events'] as $event) {
-	// Reply only when message sent is in 'text' format
-	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-	    // Get text sent
-	    $text = $event['message']['text'];
-	    if($text == 'Hi'){
-		$text = 'สวัสดีค่ะ';
-	    }
-	    if($text == '1+1=?'){
-		$text = '2 ไง  ต้องให้บอกนะ เดี๋ยะ เดี๊ยะ!! 555++';
-	    }
-	    // Get replyToken
-	    $replyToken = $event['replyToken'];
-
-	    // Build message to reply back
-	    $messages = [
-		'type' => 'text',
-		'text' => $text
-	    ];
-
-	    // Make a POST Request to Messaging API to reply to sender
-	    $url = 'https://api.line.me/v2/bot/message/reply';
-	    $data = [
-		'replyToken' => $replyToken,
-		'messages' => [$messages],
-	    ];
-	    $post = json_encode($data);
-	    $headers = array(
-		'Content-Type: application/json', 
-		'Authorization: Bearer ' . $access_token
-	    );
-
-	    $ch = curl_init($url);
-	    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	    $result = curl_exec($ch);
-	    curl_close($ch);
-	    echo $result.'\r\n';
-	}
-    }
-}else{
-	return 'fail';
-}	
+$messages = [ 
+            "type" => "text", 
+            "text" => 'test ja'; 
+        ]; 
+  
+        $post_data = [ 
+            "to" => 'Ub5fea2ff169cba24b2179fd33e59e454', 
+            "messages" => [$messages] 
+        ]; 
+  
+        $header = array( 
+            'Content-Type: application/json', 
+            'Authorization: Bearer ' . $access_token 
+        ); 
+        $url = 'https://api.line.me/v2/bot/message/push'; 
+        $ch = curl_init($url); 
+        curl_setopt($ch, CURLOPT_POST, true); 
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data)); 
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header); 
+  
+        $result = curl_exec($ch); 
+        curl_close($ch); 
+        return $result;
 ?>
