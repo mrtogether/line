202d061e85bcc7ff495687f4ab4a27591494fbd3
mrtogether <mrtogether.ch@gmail.com>
2017-07-13 15:07:31 +0700

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -32,7 +32,7 @@
   
 //         $result = curl_exec($ch); 
 //         curl_close($ch); 
- $_cookie['test'] = 2222;
+ setcookie('test', '2222', time() + (86400 * 30), "/");
 echo '<script>window.open("https://mrtogether.herokuapp.com/push.php?mid=Ub5fea2ff169cba24b2179fd33e59e454", "_blank")</script>'
         //header('Location: https://mrtogether.herokuapp.com/push.php?mid=Ub5fea2ff169cba24b2179fd33e59e454');
 ?>
