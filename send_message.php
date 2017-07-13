7c76d6c77eb4fed4052cd092a477b9ee89707a86
mrtogether <mrtogether.ch@gmail.com>
2017-07-13 13:41:27 +0700

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -2,36 +2,36 @@
 /**
  * 
  */
-$proxy = 'http://fixie:bBt21X0wwYroR2Z@velodrome.usefixie.com:80'; 
-$proxyauth = 'http://fixie:bBt21X0wwYroR2Z@velodrome.usefixie.com:80';
-$access_token = 'xjGIR1MZNjzmCI9qagfTX7ksvvmLJYmOZZfCaAvY52kld2Hm4SeDJtzRv31ZDyIum31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDtKyONakxR1kk6ADFzu3Ko5WWqxfhMcufHu3ldcWEhREAdB04t89/1O/w1cDnyilFU=';
-// Get POST body content
+// $proxy = 'http://fixie:bBt21X0wwYroR2Z@velodrome.usefixie.com:80'; 
+// $proxyauth = 'http://fixie:bBt21X0wwYroR2Z@velodrome.usefixie.com:80';
+// $access_token = 'xjGIR1MZNjzmCI9qagfTX7ksvvmLJYmOZZfCaAvY52kld2Hm4SeDJtzRv31ZDyIum31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDtKyONakxR1kk6ADFzu3Ko5WWqxfhMcufHu3ldcWEhREAdB04t89/1O/w1cDnyilFU=';
+// // Get POST body content
 
   
-        $post_data = [ 
-            "to" => 'Ub5fea2ff169cba24b2179fd33e59e454', 
-            "messages" => [ 
-                        "type" => "text", 
-                        "text" => "test ja"
-                    ]
-        ]; 
+//         $post_data = [ 
+//             "to" => 'Ub5fea2ff169cba24b2179fd33e59e454', 
+//             "messages" => [ 
+//                         "type" => "text", 
+//                         "text" => "test ja"
+//                     ]
+//         ]; 
   
-        $header = array( 
-            'Content-Type: application/json', 
-            'Authorization: Bearer ' . $access_token 
-        ); 
-        $url = 'https://api.line.me/v2/bot/message/push'; 
-        $ch = curl_init($url); 
-        curl_setopt($ch, CURLOPT_POST, true); 
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data)); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header); 
-        curl_setopt($ch, CURLOPT_PROXY, $proxy); 
-        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+//         $header = array( 
+//             'Content-Type: application/json', 
+//             'Authorization: Bearer ' . $access_token 
+//         ); 
+//         $url = 'https://api.line.me/v2/bot/message/push'; 
+//         $ch = curl_init($url); 
+//         curl_setopt($ch, CURLOPT_POST, true); 
+//         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
+//         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+//         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data)); 
+//         curl_setopt($ch, CURLOPT_HTTPHEADER, $header); 
+//         curl_setopt($ch, CURLOPT_PROXY, $proxy); 
+//         curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
   
-        $result = curl_exec($ch); 
-        curl_close($ch); 
+//         $result = curl_exec($ch); 
+//         curl_close($ch); 
         GOTO(https://mrtogether.herokuapp.com/push.php);
 ?>
 
