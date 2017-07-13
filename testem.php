a727150e793ee2a51b1c98dbbed6691c6a98b509
mrtogether <mrtogether.ch@gmail.com>
2017-07-13 16:58:32 +0700

    Update testem.php

--- testem.php
+++ testem.php
@@ -3,13 +3,12 @@ echo 4;
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL, 'http://uat.dxplace.com/dxtms/testem');
     curl_setopt($ch,CURLOPT_RETURNTRANSFER , true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode(array('a'=>'1111', 'b'=>'2222')));
+    curl_setopt($ch,CURLOPT_POSTFIELDS, 'U8c4eb5ebbd3493b74c6d17a77d3e6cd3');
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
           "Content-Type: application/json",
         )
     );
     $result = curl_exec($ch);
-var_dump($result);exit();
     $err    = curl_error($ch);
     curl_close($ch);
     if ($err) {
