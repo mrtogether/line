7c1853f16138b8cd18c64d3d21186200045b9b8b
mrtogether <mrtogether.ch@gmail.com>
2017-07-13 16:49:11 +0700

    Create testem.php

---
+++ testem.php
@@ -0,0 +1,19 @@
+<?php
+echo 3;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL, 'http://uat.dxplace.com/dxtms/testem');
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode(array('a'=>'1111', 'b'=>'2222')));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+          "Content-Type: application/json",
+        )
+    );
+    $result = curl_exec($ch);
+    $err    = curl_error($ch);
+    curl_close($ch);
+    if ($err) {
+        echo "cURL Error #:" . $err;
+    } else {
+        return $result;
+    }
+?>
