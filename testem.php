294c883c9512915918d4a3ec3117883a3b0495fb
mrtogether <mrtogether.ch@gmail.com>
2017-07-13 17:05:55 +0700

    Update testem.php

--- testem.php
+++ testem.php
@@ -1,6 +1,6 @@
 <input type="text" value="U8c4eb5ebbd3493b74c6d17a77d3e6cd3" name="mid" />
 <?php
-echo 4;
+echo 5;
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL, 'http://uat.dxplace.com/dxtms/testem');
     curl_setopt($ch,CURLOPT_RETURNTRANSFER , true);
