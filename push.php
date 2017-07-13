bbac52b4f90d046eda32f97a3404f3a6d7a036b2
mrtogether <mrtogether.ch@gmail.com>
2017-07-13 13:46:10 +0700

    Update push.php

--- push.php
+++ push.php
@@ -1,10 +1,11 @@
 <?php 
   $mid = isset($_REQUEST['mid'])? $_REQUEST['mid']: '';
+echo mid ;
 ?>
- Post to : <select multiple>
+ Post to : <select>
   <option value="U8c4eb5ebbd3493b74c6d17a77d3e6cd3">Mrtogether</option>
   <option value="saab">Saab</option>
   <option value="opel">Opel</option>
   <option value="audi">Audi</option>
-</select>
+</select><br/>
 Message : <textarea rows="4"></textarea> 
